fc3d1b7bef3d6387f76bf9ccbfdc46f3d85572bf
hrishikeshtanguturi <htanguturi@binghamton.edu>
2025-06-08 20:10:45 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkeletonComponent } from "./skeleton/skeleton.component";
+import { FormsModule } from '@angular/forms';
+import { GeminiServiceService } from './gemini-service.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkeletonComponent,FormsModule,NgFor,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chatbot';
+  prompt :string =''; 
+  loading:boolean = false;
+
+  chatHistory: any[] =[]
+constructor(){
+  this.geminiService.getMessageHistory().subscribe(
+    (res) => {
+      if(res){
+        this.chatHistory.push(res);
+      }
+    }
+  );
+}
+
+ geminiService : GeminiServiceService= inject(GeminiServiceService);
+async sendData(){
+  if(this.prompt)
+    {
+      this.loading=true;
+      const data = this.prompt;
+      this.prompt=''
+     await this.geminiService.generateText(data);
+      this.loading=false
+    }
+}
+formatText(text:string){
+  if (!text) return '';
+
+  // Replace special characters with HTML entities
+  let formattedText = text
+    .replace(/&/g, '&amp;') // Escape & as &amp;
+    .replace(/</g, '&lt;') // Escape < as &lt;
+    .replace(/>/g, '&gt;') // Escape > as &gt;
+    .replace(/\n/g, '<br>') // Convert line breaks to <br>
+    .replace(/ /g, '&nbsp;'); // Preserve spaces
+   const new_text = formattedText .replaceAll('*','');
+
+  return new_text;
+}
+
 }
---
+++ src/app/gemini-service.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { BehaviorSubject, Observable, from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeminiServiceService {
+
+  private generativeAI?: GoogleGenerativeAI;
+
+  private messageHistory: BehaviorSubject<any> = new BehaviorSubject(null);
+
+  constructor() {
+    this.generativeAI = new GoogleGenerativeAI('AIzaSyAVki0px1N5CHGjcLcnQDUUa-SKWfDdzNY')
+  }
+
+  async generateText(prompt: string) {
+    try {
+      const model = this.generativeAI?.getGenerativeModel({ model: 'gemini-2.0-flash' });
+      this.messageHistory.next({
+        from: 'user',
+        message: prompt
+      })
+
+      const result = await model?.generateContent(prompt);
+
+      if (!result || !result.response) {
+        console.error('No response received from the API.');
+        return;
+      }
+
+      const response = result.response;
+      console.log(result)
+
+      if (response.candidates && response.candidates.length > 0) {
+        const firstCandidate = response.candidates[0];
+        const response_text = firstCandidate.content.parts[0].text
+        console.log(response_text);
+        this.messageHistory.next({
+          from: 'bot',
+          message: response_text
+        })
+        return;
+      } else if (response.promptFeedback) {
+        console.error('No candidates found. Feedback:', response.promptFeedback);
+      }
+    } catch (error) {
+      console.error('Error generating text:', error);
+    }
+  }
+
+  public getMessageHistory(): Observable<any> {
+    return this.messageHistory.asObservable();
+
+
+  }
+
+
+
+}
